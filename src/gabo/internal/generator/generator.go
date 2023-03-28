255c121abbf6ee9f4990495a7bbb519bb9cd351e
Ashish Bhatia <ashishb@ashishb.net>
2023-03-27 21:01:27 -0700

    Generate mode should respect `dir` argument

--- src/gabo/internal/generator/generator.go
+++ src/gabo/internal/generator/generator.go
@@ -4,13 +4,29 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func Generate(option Option, force bool) error {
-	if force {
+type Generator struct {
+	// base dir to the git repo
+	dir string
+	// if true, overwrite existing files
+	force bool
+}
+
+func NewGenerator(dir string, force bool) Generator {
+	return Generator{
+		dir:   dir,
+		force: force,
+	}
+}
+
+func (g Generator) Generate(option Option) error {
+	if g.force {
 		log.Warn().Msgf("Force overwrite is on, existing files will be over-written")
 	}
 	switch option {
 	case _LintDocker:
-		return writeOrWarn(option.getOutputFileName(), option.getYamlConfig(), force)
+		return writeOrWarn(option.getOutputFileName(g.dir), option.getYamlConfig(), g.force)
+	default:
+		log.Fatal().Msgf("Generate for '%s' not implemented", option)
 	}
 	return nil
 }
