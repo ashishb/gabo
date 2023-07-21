272960c37378dda36d5bf15f6fc5791541107a58
Ashish Bhatia <ashishb@ashishb.net>
2023-07-21 11:45:10 -0700

    Improve docker build command

--- src/gabo/internal/generator/build_docker_generator.go
+++ src/gabo/internal/generator/build_docker_generator.go
@@ -6,7 +6,7 @@ const _buildDockerTask = `
       - name: Docker build using %s/Dockerfile
         run: |
           cd "%s"
-          DOCKER_BUILDKIT=1 docker build -f Dockerfile .
+          DOCKER_BUILDKIT=1 docker buildx build --cache-from type=gha --cache-to type=gha  -f Dockerfile .
 `
 
 func generateBuildDockerYaml(repoDir string) (*string, error) {
