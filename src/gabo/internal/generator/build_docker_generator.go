0c612e6f91166bcdf098c8b857b8ada2e6e1110e
Ashish Bhatia <ashishb@ashishb.net>
2025-04-27 00:26:21 -0700

    feat: add support for `dive` for preventing inefficient images (#129)

--- src/gabo/internal/generator/build_docker_generator.go
+++ src/gabo/internal/generator/build_docker_generator.go
@@ -6,7 +6,19 @@ const _buildDockerTask = `
       - name: Docker build using %s/Dockerfile
         working-directory: "%s"
         run: |
-          DOCKER_BUILDKIT=1 docker buildx build --cache-from type=gha --cache-to type=gha  -f Dockerfile .
+          DOCKER_BUILDKIT=1 docker buildx build --cache-from type=gha --cache-to type=gha -t tmp1 -f Dockerfile .
+`
+
+const _checkDockerWithDiveTask = `
+	  - name: Check Docker image (%s/Dockerfile) with dive for wasted space
+        working-directory: "%s"
+		run: |
+			docker run --rm \
+				-v /var/run/docker.sock:/var/run/docker.sock \
+				--env=CI=true \
+				--network none \
+				docker.io/wagoodman/dive:latest \
+				tmp1
 `
 
 func generateBuildDockerYaml(repoDir string) (*string, error) {
@@ -16,7 +28,8 @@ func generateBuildDockerYaml(repoDir string) (*string, error) {
 	}
 	str := _buildDockerTemplate
 	for _, dir := range dirs {
-		str += fmt.Sprintf(_buildDockerTask, dir, dir)
+		str += fmt.Sprintf(_buildDockerTask+"\n\n", dir, dir)
+		str += fmt.Sprintf(_checkDockerWithDiveTask, dir, dir)
 	}
 	return &str, nil
 }
